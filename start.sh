e5e0d627cd520df04faa0e005167ac5b7229131e
Hans Ole Gjerdrum <hog@eika.no>
2022-11-08 08:33:43 +0100

    New style theme and new Systek logo

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
 ./node_modules/.bin/reveal-md presentasjon.md \
-          -w \
-          --disable-auto-open \
-          --theme theme/systek-white.css \
-          --highlight-theme dracula
+    -w \
+    --disable-auto-open \
+    --theme theme/systek-night.css \
+    --title: "Test for Fun" \
+    --highlight-theme nord
